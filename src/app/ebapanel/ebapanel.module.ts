1956b34878a9b270c9dbf5a44eca417d5f200bba
Sarthakprusty <prustysarthak123@gmail.com>
2023-11-17 22:50:25 +0530

    sarthak frontend 17-11-23 eba

---
+++ src/app/ebapanel/ebapanel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EbapanelRoutingModule } from './ebapanel-routing.module';
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        EbapanelRoutingModule
+    ]
+})
+export class EbapanelModule { }
