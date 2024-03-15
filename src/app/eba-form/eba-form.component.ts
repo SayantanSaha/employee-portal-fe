bf746ace37388e4531c8f7efc56fb1fa69f707ae
Sayantan Saha <sayantan.somu@gmail.com>
2024-03-15 15:16:42 +0530

    Merge branch 'master' of https://github.com/SayantanSaha/employee-portal-fe

--- src/app/eba-form/eba-form.component.ts
+++ src/app/eba-form/eba-form.component.ts
@@ -188,17 +188,29 @@ export class EbaFormComponent {
     if((this.employee!.family && this.employee!.family.length>0) || (this.employee!.closefamily && this.employee!.closefamily.length>0)) {
       const fiveYearsLater = new Date();
       fiveYearsLater.setFullYear(today.getFullYear() + 5);
-
-      // Format the date as 'yyyy-MM-dd' (the format that input type date expects)
       const formattedDate = this.formatDate(fiveYearsLater);
+
+      const onemonthLater = new Date();
+        onemonthLater.setMonth(onemonthLater.getMonth() + 1);
+      const onemonthformat = this.formatDate(onemonthLater);
 
       this.employee!.family?.forEach(member => {
         member.pivot.eba_passes?.forEach(ebapass=>{
-          if(!ebapass.eba_pass_exp_date_edited && formattedDate<=this.employee!.dor && this.employee?.dh){
-            ebapass.eba_pass_exp_date_edit=formattedDate;
+          if (member.id !== 27){
+            if(!ebapass.eba_pass_exp_date_edited && formattedDate<=this.employee!.dor && this.employee?.dh){
+              ebapass.eba_pass_exp_date_edit=formattedDate;
+            }
+            if(formattedDate>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
+              ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+            }
           }
-          if(formattedDate>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
-            ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+          if (member.id === 27){
+            if(!ebapass.eba_pass_exp_date_edited && onemonthformat<=this.employee!.dor && this.employee?.dh){
+              ebapass.eba_pass_exp_date_edit=onemonthformat;
+            }
+            if(onemonthformat>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
+              ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+            }
           }
         })
       })
@@ -217,6 +229,9 @@ export class EbaFormComponent {
       const oneYearsLater = new Date();
       oneYearsLater.setFullYear(today.getFullYear() + 1);
       const formattedDate = this.formatDate(oneYearsLater);
+      const onemonthLater = new Date();
+      onemonthLater.setMonth(onemonthLater.getMonth() + 1);
+      const onemonthformat = this.formatDate(onemonthLater);
       this.employee!.servants?.forEach(servant => {
         servant.eba_passes?.forEach(ebapass=>{
           if(!ebapass.eba_pass_exp_date_edited && formattedDate<=this.employee!.dor && servant.Selected_dh && this.employee?.dh){
@@ -228,11 +243,21 @@ export class EbaFormComponent {
         })
         servant.relations?.forEach(member=>{
           member.pivot.eba_passes?.forEach(ebapass=>{
-            if(!ebapass.eba_pass_exp_date_edited && formattedDate<=this.employee!.dor && this.employee?.dh){
-              ebapass.eba_pass_exp_date_edit=formattedDate;
+            if (member.id !== 27){
+              if(!ebapass.eba_pass_exp_date_edited && formattedDate<=this.employee!.dor && this.employee?.dh){
+                ebapass.eba_pass_exp_date_edit=formattedDate;
+              }
+              if(formattedDate>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
+                ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+              }
             }
-            if(formattedDate>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
-              ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+            if (member.id === 27){
+              if(!ebapass.eba_pass_exp_date_edited && onemonthformat<=this.employee!.dor && this.employee?.dh){
+                ebapass.eba_pass_exp_date_edit=onemonthformat;
+              }
+              if(onemonthformat>this.employee!.dor && this.employee?.dh && !ebapass.eba_pass_exp_date_edited){
+                ebapass.eba_pass_exp_date_edit=this.employee!.dor;
+              }
             }
           })
         })
@@ -1105,6 +1130,7 @@ export class EbaFormComponent {
           return;
         }
       }
+
       else if (this.user && this.user.role && this.user.role.some((role:  number ) => (role === 5 || role == 6|| role == 9|| role == 10))) {
         this.employeeService.updateebastatus(id, 'Approve', this.remark?? '', this.file_path_64 ?? '').subscribe(
             () =>{
@@ -1153,6 +1179,58 @@ export class EbaFormComponent {
       return;
     }
   }
+
+  Forwardapplication(){
+    const id = +this.route.snapshot.params['id'];
+    if (!isNaN(id)) {if (this.user && this.user.role && this.user.role.some((role:  number ) => (role == 9))) {
+      this.employeeService.updateebastatus(id, 'Forward', this.remark?? '', this.file_path_64 ?? '').subscribe(
+          () =>{
+            Swal.fire({
+              icon: 'success',
+              title: 'Success',
+              text: 'Approved successfully',
+            }).then(() => {
+              // Redirect to the dashboard route
+              this.router.navigate(['ebapanel']);
+            });
+          },
+          (error) => {
+            console.log(error);
+            console.log(error.status);
+            console.log(error.error);
+            if (error.status === 302) {
+              Swal.fire({
+                icon: 'warning',
+                title: 'Warning',
+                text: 'You are not authorised !!!',
+              });
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'An error occurred while Approving application status.',
+              });
+            }
+          }
+      );
+    } else{
+      Swal.fire({
+        icon: 'warning',
+        title: 'Warning',
+        text: 'You are not authorised !!!',
+      });
+    }
+    }else{
+      console.error('ID parameter is missing or invalid in the URL.');
+      Swal.fire({
+        icon: 'warning',
+        title: 'Warning',
+        text: 'id missing !!!',
+      });
+      return;
+    }
+  }
+
 
   returnapplication()
   {
