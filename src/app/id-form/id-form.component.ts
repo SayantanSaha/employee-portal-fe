49a049a8513fa99ac8d53ebafcc81e931df0276f
Sarthak <sarthak@rb.nic.in>
2024-05-30 16:45:51 +0530

    changes

--- src/app/id-form/id-form.component.ts
+++ src/app/id-form/id-form.component.ts
@@ -1,31 +1,25 @@
-import {Component, Inject} from '@angular/core';
-import {User} from "../model/User";
-import {Employee} from "../model/Employee";
-import {EmployeeService} from "../employee.service";
-import {ActivatedRoute} from "@angular/router";
-import Swal from "sweetalert2";
-import {environment} from "../../environments/environment";
-import {Idcards} from "../model/Idcards";
-import {fileToBase64} from "../profile/fileToBase64";
-import { Router } from '@angular/router';
-import {DatePipe} from "@angular/common";
-declare var jQuery: any;
+import { Component, NgModule, OnInit, Inject } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { environment } from '../../environments/environment';
+import { Employee } from '../model/Employee';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-id-form',
-  standalone: true,
-  imports: [
-    DatePipe
-  ],
   templateUrl: './id-form.component.html',
-  styleUrl: './id-form.component.scss'
+  styleUrls: ['./id-form.component.scss'],
+  providers: [DatePipe] // Include DatePipe in the providers array
 })
-export class IdFormComponent {
+export class IdFormComponent implements OnInit {
   baseUrl: string = '';
   constructor(
-    @Inject('BASE_URL') baseUrl: string,private employeeService: EmployeeService,
+    @Inject('BASE_URL') baseUrl: string,
+    private employeeService: EmployeeService,
     private route: ActivatedRoute,
-    private router: Router,
+    private router: Router
   ) {
     this.baseUrl = baseUrl;
   }
@@ -39,44 +33,42 @@ export class IdFormComponent {
   id: string | null = null;
   remark: string | null = null;
   file_path: string | null = null;
-  file_path_64:string|null=null;
+  file_path_64: string | null = null;
   isLoading: boolean = false;
 
-
-    entryPass = {
-        name: '',
-        fathersName: '',
-        designation: '',
-        mobileNumber: '',
-        permanentAddress: '',
-        presentAddress: '',
-        workArea: '',
-        contractualCompanyName: '',
-        periodOfWork: '',
-        policeVerificationNo: '',
-        recommendingLetterNo: '',
-        dateOfBirth: '',
-        gender: '',
-        identificationMark: '',
-        signatureOfApplicant: '',
-        gazettedOfficerSignature: '',
-        gazettedOfficerName: '',
-        gazettedOfficerDesignation: '',
-        imageUrl:''
-    };
-
+  entryPass = {
+    name: '',
+    fathersName: '',
+    designation: '',
+    mobileNumber: '',
+    permanentAddress: '',
+    presentAddress: '',
+    workArea: '',
+    contractualCompanyName: '',
+    periodOfWork: '',
+    policeVerificationNo: '',
+    recommendingLetterNo: '',
+    dateOfBirth: '',
+    gender: '',
+    identificationMark: '',
+    signatureOfApplicant: '',
+    gazettedOfficerSignature: '',
+    gazettedOfficerName: '',
+    gazettedOfficerDesignation: '',
+    imageUrl: ''
+  };
 
   ngOnInit() {
-    this.isLoading=true;
+    this.isLoading = true;
     this.mode = this.route.snapshot.paramMap.get('mode');
     this.setEditable(this.mode == 'edit');
 
     let userString: string | null = sessionStorage.getItem('user') != null ? sessionStorage.getItem('user') : '[]';
     this.user = JSON.parse(userString!);
 
-    if (this.user && this.user.role && this.user.role.some((role: number) => (role === 11 || role == 12 || role == 13 ))) {
+    if (this.user && this.user.role && this.user.role.some((role: number) => (role === 11 || role == 12 || role == 13))) {
       this.id = this.route.snapshot.paramMap.get('id');
-      if(this.mode !== 'return'){
+      if (this.mode !== 'return') {
         this.letverify(!isNaN(+this.id!));
       }
     }
@@ -85,7 +77,7 @@ export class IdFormComponent {
       // 'id' is present, try to convert it to a number
       const idNumber = +this.id;
       if (!isNaN(idNumber)) {
-        if (this.user && this.user.role && this.user.role.some((role: number) => (role === 11 || role == 12 || role == 13 ))) {
+        if (this.user && this.user.role && this.user.role.some((role: number) => (role === 11 || role == 12 || role == 13))) {
           // 'id' is a valid number, call getEbaProfile
           this.employeeService.getEbaProfile(idNumber).subscribe(
             (data: any) => {
@@ -98,12 +90,8 @@ export class IdFormComponent {
         this.employeeService.getMyIDProfile().subscribe(
           (data: any) => {
             this.employee = data;
-            // @ts-ignore
-            // this.getDistricts(this.employee.curr_state!).then(districts => this.currDistricts = districts);
-            // // @ts-ignore
-            // this.getDistricts(this.employee.perm_state!).then(districts => this.permDistricts = districts);
-          },(error) => {
-            if (error.status === 400 && error.error.msg === "Not allowed") {
+          }, (error) => {
+            if (error.status === 400 && error.error.msg === 'Not allowed') {
               Swal.fire({
                 title: 'Not Allowed',
                 text: 'You are not allowed to access this resource.',
@@ -120,12 +108,8 @@ export class IdFormComponent {
       this.employeeService.getMyIDProfile().subscribe(
         (data: any) => {
           this.employee = data;
-          // @ts-ignore
-          // this.getDistricts(this.employee.curr_state!).then(districts => this.currDistricts = districts);
-          // // @ts-ignore
-          // this.getDistricts(this.employee.perm_state!).then(districts => this.permDistricts = districts);
-        },(error) => {
-          if (error.status === 400 && error.error.msg === "Not allowed") {
+        }, (error) => {
+          if (error.status === 400 && error.error.msg === 'Not allowed') {
             Swal.fire({
               title: 'Not Allowed',
               text: 'You are not allowed to access this resource.',
@@ -137,14 +121,14 @@ export class IdFormComponent {
         }
       );
     }
-    this.isLoading=false;
+    this.isLoading = false;
   }
 
-  setEditable(status:boolean){
+  setEditable(status: boolean) {
     this.editable = status;
   }
 
-  letverify(status:boolean){
+  letverify(status: boolean) {
     this.urlId = status;
   }
 
@@ -153,3 +137,14 @@ export class IdFormComponent {
   }
 
 }
+
+@NgModule({
+  declarations: [
+    IdFormComponent // Include your component declaration here
+  ],
+  imports: [
+    CommonModule // Import CommonModule here
+    // Other modules imports
+  ]
+})
+export class IdFormComponentModule { }
