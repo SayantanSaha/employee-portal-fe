c1ce8829a06925120c7edb5edfb798744c5c8e41
Sarthak <sarthak@rb.nic.in>
2024-06-19 13:33:04 +0530

    19.6.24

--- src/app/ebaformview/ebaformview.component.ts
+++ src/app/ebaformview/ebaformview.component.ts
@@ -1,13 +1,13 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { ActivatedRoute } from '@angular/router';
 import { Employee } from '../model/Employee';
 import { Router } from '@angular/router';
-import {Designation} from "../model/Designation";
-import {Division} from "../model/Division";
-import {Idcards} from "../model/Idcards";
+import { Designation } from "../model/Designation";
+import { Division } from "../model/Division";
+import { Idcards } from "../model/Idcards";
 import Swal from "sweetalert2";
-import {environment} from "../../environments/environment";
+import { environment } from "../../environments/environment";
 
 
 
@@ -22,30 +22,30 @@ export class EbaformviewComponent implements OnInit {
     private employeeService: EmployeeService,
     private route: ActivatedRoute,
     private router: Router
-  ) {}
+  ) { }
 
   employee: any;
   apiUrl = environment.apiUrl;
-  fromUrl: string='';
-  submit: string='';
+  fromUrl: string = '';
+  submit: string = '';
   id: any;
-  status:string='';
-  reg_no:string='';
+  status: string = '';
+  reg_no: string = '';
   isLoading: boolean = false;
 
   ngOnInit(): void {
     // Retrieve data from router state
     this.employee = history.state.employeeData;
     console.log(this.employee);
-    this.fromUrl=history.state.fromUrl;
+    this.fromUrl = history.state.fromUrl;
     console.log(this.fromUrl);
-    this.submit=history.state.submit;
+    this.submit = history.state.submit;
     console.log(this.submit);
-    this.id=history.state.id;
+    this.id = history.state.id;
     console.log(this.id);
-    this.status=history.state.status;
+    this.status = history.state.status;
     console.log(this.status);
-    this.reg_no=history.state.reg_no;
+    this.reg_no = history.state.reg_no;
     console.log(this.reg_no);
   }
 
@@ -176,81 +176,81 @@ export class EbaformviewComponent implements OnInit {
       //       }
       //   );
       // } else {
-        this.isLoading=true;
-        // Send the modified employee object to the server
-        this.employeeService.applyeba(this.employee).subscribe(
-          // this.employeeService.applyeba(Employee).subscribe(
-          // if (this.validationErrors.length > 0) {
-          //
-          //   const errorMessage = this.validationErrors
-          //       .map((error, index) => `${index + 1}. ${error}`)
-          //       .join('\n');
-          //
-          //   Swal.fire({
-          //     icon: 'error',
-          //     title: 'Error',
-          //     html: errorMessage.replace(/\n/g, '<br/>'),
-          //     width: 'auto', // Adjust as needed
-          //   });
-          //   return; // Exit without calling the API
-          // }
+      this.isLoading = true;
+      // Send the modified employee object to the server
+      this.employeeService.applyeba(this.employee).subscribe(
+        // this.employeeService.applyeba(Employee).subscribe(
+        // if (this.validationErrors.length > 0) {
+        //
+        //   const errorMessage = this.validationErrors
+        //       .map((error, index) => `${index + 1}. ${error}`)
+        //       .join('\n');
+        //
+        //   Swal.fire({
+        //     icon: 'error',
+        //     title: 'Error',
+        //     html: errorMessage.replace(/\n/g, '<br/>'),
+        //     width: 'auto', // Adjust as needed
+        //   });
+        //   return; // Exit without calling the API
+        // }
 
-          // Validation true then Api call otherwise please check
-          // data=>console.log(data),
-          // error=>console.log(error)
+        // Validation true then Api call otherwise please check
+        // data=>console.log(data),
+        // error=>console.log(error)
 
 
-          data => {
-            this.isLoading = false;
-            console.log(data);
+        data => {
+          this.isLoading = false;
+          console.log(data);
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Eba application applied successfully and pending for approval',
+            // }).then((result) => {
+            //   if (result.isConfirmed) {
+            //     // Redirect to the desired page
+            //     window.location.reload();
+            //   }
+          }).then(() => {
+            this.fromUrl = '';
+            this.router.navigate(['dashboard']);
+          });
+        },
+        (error) => {
+          this.isLoading = false;
+          console.log(error);
+          console.log(error.status);
+          console.log(error.error);
+          if (error) {
+            // if(error.status === 302){
+            //   Swal.fire({
+            //     icon: 'warning',
+            //     title: 'Warning',
+            //     text: 'Previous Record is still pending !!!',
+            //   });
+            // }else if(error.status === 303){
+            //   Swal.fire({
+            //     icon: 'warning',
+            //     title: 'Warning',
+            //     text: 'you already have a approved application',
+            //   });
+            // }
+            // else{
             Swal.fire({
-              icon: 'success',
-              title: 'Success',
-              text: 'Eba application applied successfully and pending for approval',
-              // }).then((result) => {
-              //   if (result.isConfirmed) {
-              //     // Redirect to the desired page
-              //     window.location.reload();
-              //   }
-            }).then(() => {
-              this.fromUrl = '';
-              this.router.navigate(['dashboard']);
+              icon: 'error',
+              title: 'API Error',
+              text: 'An error occurred while updating.',
             });
-          },
-          (error) => {
-            this.isLoading = false;
-            console.log(error);
-            console.log(error.status);
-            console.log(error.error);
-            if (error) {
-              // if(error.status === 302){
-              //   Swal.fire({
-              //     icon: 'warning',
-              //     title: 'Warning',
-              //     text: 'Previous Record is still pending !!!',
-              //   });
-              // }else if(error.status === 303){
-              //   Swal.fire({
-              //     icon: 'warning',
-              //     title: 'Warning',
-              //     text: 'you already have a approved application',
-              //   });
-              // }
-              // else{
-              Swal.fire({
-                icon: 'error',
-                title: 'API Error',
-                text: 'An error occurred while updating.',
-              });
-            }
           }
-        );
+        }
+      );
       // }
     }
   }
 
 
-  updateEba(){
+  updateEba() {
     if (this.employee) {
       if (this.employee.designations === null || this.employee.designations.length === 0) {
         Swal.fire({
@@ -287,52 +287,52 @@ export class EbaformviewComponent implements OnInit {
         });
         return;
       }
-      this.isLoading=true;
+      this.isLoading = true;
       this.employeeService.updateeba(this.employee, this.id).subscribe(
-          data => {
-            this.isLoading = false;
-            console.log(data);
+        data => {
+          this.isLoading = false;
+          console.log(data);
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Eba application applied successfully and pending for approval',
+            // }).then((result) => {
+            //   if (result.isConfirmed) {
+            //     // Redirect to the desired page
+            //     window.location.reload();
+            //   }
+          }).then(() => {
+            this.fromUrl = '';
+            this.router.navigate(['dashboard']);
+          });
+        },
+        (error) => {
+          this.isLoading = false;
+          console.log(error);
+          console.log(error.status);
+          console.log(error.error);
+          if (error) {
+            // if(error.status === 302){
+            //   Swal.fire({
+            //     icon: 'warning',
+            //     title: 'Warning',
+            //     text: 'Previous Record is still pending !!!',
+            //   });
+            // }else if(error.status === 303){
+            //   Swal.fire({
+            //     icon: 'warning',
+            //     title: 'Warning',
+            //     text: 'you already have a approved application',
+            //   });
+            // }
+            // else{
             Swal.fire({
-              icon: 'success',
-              title: 'Success',
-              text: 'Eba application applied successfully and pending for approval',
-              // }).then((result) => {
-              //   if (result.isConfirmed) {
-              //     // Redirect to the desired page
-              //     window.location.reload();
-              //   }
-            }).then(() => {
-              this.fromUrl = '';
-              this.router.navigate(['dashboard']);
+              icon: 'error',
+              title: 'API Error',
+              text: 'An error occurred while updating.',
             });
-          },
-          (error) => {
-            this.isLoading = false;
-            console.log(error);
-            console.log(error.status);
-            console.log(error.error);
-            if (error) {
-              // if(error.status === 302){
-              //   Swal.fire({
-              //     icon: 'warning',
-              //     title: 'Warning',
-              //     text: 'Previous Record is still pending !!!',
-              //   });
-              // }else if(error.status === 303){
-              //   Swal.fire({
-              //     icon: 'warning',
-              //     title: 'Warning',
-              //     text: 'you already have a approved application',
-              //   });
-              // }
-              // else{
-              Swal.fire({
-                icon: 'error',
-                title: 'API Error',
-                text: 'An error occurred while updating.',
-              });
-            }
           }
+        }
       );
 
     }
