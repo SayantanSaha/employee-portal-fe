a88d557d85f9ec9d8bcc9c5ed0ea07598216805c
Sarthak <sarthak@rb.nic.in>
2024-12-18 15:08:28 +0530

    18.12.24

--- src/app/rbcardapplyform/rbcardapplyform.component.ts
+++ src/app/rbcardapplyform/rbcardapplyform.component.ts
@@ -10,17 +10,17 @@ import { District } from "../model/District";
 import { fileToBase64 } from "../profile/fileToBase64";
 import { environment } from "../../environments/environment";
 import { Designation } from "../model/Designation";
-import {Inject} from '@angular/core';
-import {User} from "../model/User";
+import { Inject } from '@angular/core';
+import { User } from "../model/User";
 
-import {ActivatedRoute} from "@angular/router";
-import {Division} from "../model/Division";
-import {Relation} from "../model/Relation";
-import {Servants} from "../model/Servants";
+import { ActivatedRoute } from "@angular/router";
+import { Division } from "../model/Division";
+import { Relation } from "../model/Relation";
+import { Servants } from "../model/Servants";
 
-import {ServantRel} from "../model/ServantRel";
-import {Idcards} from "../model/Idcards";
-import {Vehicles} from "../model/Vehicles";
+import { ServantRel } from "../model/ServantRel";
+import { Idcards } from "../model/Idcards";
+import { Vehicles } from "../model/Vehicles";
 declare var jQuery: any;
 
 
@@ -35,7 +35,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrl: './rbcardapplyform.component.scss'
 })
 export class RbcardapplyformComponent {
-  employee: Employee = new Employee();
+  employee: Employee | null = new Employee();
   password: string = '';
   cpassword: string = '';
   organization: string = '';
@@ -49,6 +49,7 @@ export class RbcardapplyformComponent {
   bloodGroups: string[] = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
   passColors: any[] = [];
   payLevels: any[] = [];
+  showform: boolean = false;
   passwordsMatch: boolean = false;
   passwordValid: boolean = false;
   passwordTouched: boolean = false;
@@ -59,10 +60,10 @@ export class RbcardapplyformComponent {
   currentPageIndex = 0;
   totalPages = 5;
   slides = new Array(this.totalPages);
-  currentDate: string= "";
+  currentDate: string = "";
   isLoading: boolean = false;
-  user:User = new User();
-  id:any;
+  user: User = new User();
+  id: any;
 
   constructor(
     private employeeService: EmployeeService,
@@ -78,10 +79,10 @@ export class RbcardapplyformComponent {
   ngOnInit() {
     const userString: string | null = sessionStorage.getItem('user');
     this.user = userString ? JSON.parse(userString) : [];
-    this.id=this.user.id;
+    this.id = this.user.id;
 
 
-    this.isLoading=true;
+    this.isLoading = true;
     const today = new Date();
     today.setFullYear(today.getFullYear() - 18); // Subtract 18 years from today
 
@@ -117,15 +118,15 @@ export class RbcardapplyformComponent {
     );
 
     this.employeeService.getDesignations(1).subscribe(
-      data=>this.designations=data,
+      data => this.designations = data,
       error => console.log(error)
     );
 
     this.employeeService.getDivisionMasterList().subscribe(
-      data=>this.divisiontypelist=data,
+      data => this.divisiontypelist = data,
       error => console.log(error)
     );
-    this.isLoading=false;
+    this.isLoading = false;
   }
 
 
@@ -137,7 +138,7 @@ export class RbcardapplyformComponent {
     this.isLoading = true;
 
     this.employeeService.rbformapply(
-      this.employee, // Pass the employee object
+      this.employee!, // Pass the employee object
 
     ).subscribe(
       data => {
@@ -169,11 +170,11 @@ export class RbcardapplyformComponent {
 
   cPAddress() {
     if (this.isCpAddressChecked) {
-      if (this.employee.curr_add && this.employee.curr_pin && this.employee.curr_state && this.employee.curr_district) {
-        this.employee.perm_add = this.employee.curr_add;
-        this.employee.perm_pin = this.employee.curr_pin;
-        this.employee.perm_state = this.employee.curr_state;
-        this.employee.perm_district = this.employee.curr_district;
+      if (this.employee!.curr_add && this.employee!.curr_pin && this.employee!.curr_state && this.employee!.curr_district) {
+        this.employee!.perm_add = this.employee!.curr_add;
+        this.employee!.perm_pin = this.employee!.curr_pin;
+        this.employee!.perm_state = this.employee!.curr_state;
+        this.employee!.perm_district = this.employee!.curr_district;
 
         this.permDistricts = [...this.currDistricts];
       } else {
@@ -185,10 +186,10 @@ export class RbcardapplyformComponent {
         });
       }
     } else {
-      this.employee.perm_add = null;
-      this.employee.perm_pin = null;
-      this.employee.perm_state = null;
-      this.employee.perm_district = null;
+      this.employee!.perm_add = null;
+      this.employee!.perm_pin = null;
+      this.employee!.perm_state = null;
+      this.employee!.perm_district = null;
     }
   }
 
@@ -204,7 +205,7 @@ export class RbcardapplyformComponent {
         if (fileType === 'application/pdf' && fileSize <= 1048576) {
           const base64String: string = await fileToBase64(selectedFile); // Convert the file to base64
           if (this.employee) {
-            this.employee.postingOrder = base64String;
+            this.employee!.postingOrder = base64String;
           } else {
             console.log('this.employee is null.');
           }
@@ -230,8 +231,8 @@ export class RbcardapplyformComponent {
   }
 
   calculateDor(): void {
-    if (this.employee.dob && this.employee.emp_type!='Temporary') {
-      const dob = new Date(this.employee.dob);
+    if (this.employee!.dob && this.employee!.emp_type != 'Temporary') {
+      const dob = new Date(this.employee!.dob);
       let date = new Date(dob);
 
       // Add 60 years to the Date of Birth
@@ -247,23 +248,50 @@ export class RbcardapplyformComponent {
       }
 
       // Format DoR as 'YYYY-MM-DD' for the input
-      this.employee.dor = date.toISOString().slice(0, 10);
+      this.employee!.dor = date.toISOString().slice(0, 10);
     }
   }
 
   idCardDetails(): void {
-    if(this.employee.id_cards != null){
-    this.employeeService.getidCardDetails(this.employee).subscribe(
-      data=>this.employee=data,
-      error => console.log(error)
-    )
-  }
+    if (this.employee?.id_cards != null) {
+      if (this.employee) {
+        const applyReason = this.employee.apply_reason;
+        const idcard = this.employee.id_cards;
+        const emptype = this.employee.emp_type;
+        this.employee = new Employee();
+        this.employee.apply_reason = applyReason;
+        this.employee.id_cards = idcard;
+        this.employee.emp_type = emptype;
+      }
+
+      this.employeeService.getidCardDetails(this.employee).subscribe(
+        data => {
+          this.employee = data;
+          this.showform = true;
+        },
+        error => {
+          if (error.status === 404) {
+            // Handle 404 error
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: error.error?.msg || 'ID card not found',
+            });
+          } else {
+            // Handle other errors if needed
+            console.error(error);
+          }
+          this.showform = false;
+        }
+      );
+    }
   }
 
+
   empTypeCondition(): void {
-    if ( this.employee.emp_type=='Temporary') {
-      delete this.employee.dor;
-      delete this.employee.payLevel;
+    if (this.employee!.emp_type == 'Temporary') {
+      delete this.employee!.dor;
+      delete this.employee!.payLevel;
     }
   }
 
@@ -271,6 +299,11 @@ export class RbcardapplyformComponent {
   onApplyReasonChange(): void {
     if (this.employee!.apply_reason !== 'Lost/theaft') {
       this.employee!.FIR_no = null;
+    }if (this.employee) {
+      const applyReason = this.employee.apply_reason; // Preserve apply_reason
+      this.employee = new Employee(); // Reset the employee object
+      this.employee.apply_reason = applyReason; // Restore apply_reason
+      this.showform = false;
     }
   }
 
@@ -302,7 +335,7 @@ export class RbcardapplyformComponent {
         if (fileType === 'application/pdf' && fileSize <= 1048576) {
           const base64String: string = await fileToBase64(selectedFile); // Convert the file to base64
           if (this.employee) {
-            this.employee.id_proof = base64String;
+            this.employee!.id_proof = base64String;
           } else {
             console.log('this.employee is null.');
           }
@@ -330,11 +363,11 @@ export class RbcardapplyformComponent {
   updateDesgPrint(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const selectedOption = selectElement.options[selectElement.selectedIndex];
-    const selectedDesg = this.designations.find(desg => desg.id === this.employee.designation);
+    const selectedDesg = this.designations.find(desg => desg.id === this.employee!.designation);
     if (selectedDesg) {
-      this.employee.desg_print = selectedDesg.desg_desc;
+      this.employee!.desg_print = selectedDesg.desg_desc;
     } else {
-      this.employee.desg_print = '';
+      this.employee!.desg_print = '';
     }
   }
 
@@ -358,9 +391,9 @@ export class RbcardapplyformComponent {
             const base64String: string = await fileToBase64(file); // Convert the file to base64
             if (this.employee) {
               if (param === 'Profile Photo') {
-                this.employee.profile_photo = base64String;
+                this.employee!.profile_photo = base64String;
               } else if (param === 'Employee Sign') {
-                this.employee.sign_path = base64String;
+                this.employee!.sign_path = base64String;
               }
             } else {
               console.log('this.employee is null.');
@@ -391,18 +424,18 @@ export class RbcardapplyformComponent {
 
 
 
-  onInput(event: any , property:string) {
+  onInput(event: any, property: string) {
     const input = event.target.value;
     // Remove non-numeric characters using regular expression
     const numericInput = input.replace(/\D/g, '');
     // Update the input value with the filtered numeric input
     event.target.value = numericInput;
     // Update the ngModel binding
-    if(property=='curr_pin')
+    if (property == 'curr_pin')
       this.employee!.curr_pin = numericInput;
-    if(property=='perm_pin')
+    if (property == 'perm_pin')
       this.employee!.perm_pin = numericInput;
-    if(property=='mobile')
+    if (property == 'mobile')
       this.employee!.mobile = numericInput;
 
   }
@@ -421,58 +454,58 @@ export class RbcardapplyformComponent {
 
   openPopup() {
     this.display = "block";
-    if (this.employee.designation) {
-      this.employee.designation = this.designations.find(desg => desg.id === this.employee.designation);
+    if (this.employee!.designation) {
+      this.employee!.designation = this.designations.find(desg => desg.id === this.employee!.designation);
     }
-    if (this.employee.curr_state) {
-      this.employee.curr_state= this.states.find(state => state.id === this.employee.curr_state);
+    if (this.employee!.curr_state) {
+      this.employee!.curr_state = this.states.find(state => state.id === this.employee!.curr_state);
     }
-    if (this.employee.curr_district) {
-      this.employee.curr_district = this.currDistricts.find(dist => dist.id === this.employee.curr_district);
+    if (this.employee!.curr_district) {
+      this.employee!.curr_district = this.currDistricts.find(dist => dist.id === this.employee!.curr_district);
     }
-    if (this.employee.perm_state) {
-      this.employee.perm_state = this.states.find(state => state.id === this.employee.perm_state);
+    if (this.employee!.perm_state) {
+      this.employee!.perm_state = this.states.find(state => state.id === this.employee!.perm_state);
     }
-    if (this.employee.perm_district) {
-      this.employee.perm_district = this.permDistricts.find(dist => dist.id === this.employee.perm_district);
+    if (this.employee!.perm_district) {
+      this.employee!.perm_district = this.permDistricts.find(dist => dist.id === this.employee!.perm_district);
     }
-    if (this.employee.division) {
-      this.employee.division = this.divisiontypelist.find(div => div.id === this.employee.division);
+    if (this.employee!.division) {
+      this.employee!.division = this.divisiontypelist.find(div => div.id === this.employee!.division);
     }
-    if (this.employee.organization) {
-      this.employee.organization = this.orglist.find(org => org.id === this.employee.organization);
+    if (this.employee!.organization) {
+      this.employee!.organization = this.orglist.find(org => org.id === this.employee!.organization);
     }
-    if (this.employee.payLevel) {
-      this.employee.payLevel = this.payLevels.find(pay => pay.id === this.employee.payLevel);
+    if (this.employee!.payLevel) {
+      this.employee!.payLevel = this.payLevels.find(pay => pay.id === this.employee!.payLevel);
     }
 
 
   }
   closePopup() {
     this.display = "none";
-    if (this.employee.designation) {
-      this.employee.designation = this.employee.designation.id;
+    if (this.employee!.designation) {
+      this.employee!.designation = this.employee!.designation.id;
     }
-    if (this.employee.curr_state) {
-      this.employee.curr_state = this.employee.curr_state.id;
+    if (this.employee!.curr_state) {
+      this.employee!.curr_state = this.employee!.curr_state.id;
     }
-    if (this.employee.curr_district) {
-      this.employee.curr_district = this.employee.curr_district.id;
+    if (this.employee!.curr_district) {
+      this.employee!.curr_district = this.employee!.curr_district.id;
     }
-    if (this.employee.perm_state) {
-      this.employee.perm_state = this.employee.perm_state.id;
+    if (this.employee!.perm_state) {
+      this.employee!.perm_state = this.employee!.perm_state.id;
     }
-    if (this.employee.perm_district) {
-      this.employee.perm_district = this.employee.perm_district.id;
+    if (this.employee!.perm_district) {
+      this.employee!.perm_district = this.employee!.perm_district.id;
     }
-    if (this.employee.division) {
-      this.employee.division = this.employee.division.id;
+    if (this.employee!.division) {
+      this.employee!.division = this.employee!.division.id;
     }
-    if (this.employee.organization) {
-      this.employee.organization = this.employee.organization.id;
+    if (this.employee!.organization) {
+      this.employee!.organization = this.employee!.organization.id;
     }
-    if (this.employee.payLevel) {
-      this.employee.payLevel = this.employee.payLevel.id;
+    if (this.employee!.payLevel) {
+      this.employee!.payLevel = this.employee!.payLevel.id;
     }
 
   }
